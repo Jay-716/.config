5c9f1d1d3f40576f9df72e0b3f54a81d570c8afe
Jay-716 <13422525511@163.com>
2023-02-13 13:17:12 +0800

    Update fish config.

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,19 @@
-set -x GPG_TTY (tty)
-set -x EDITOR /usr/bin/nvim
+if status is-interactive
+    set -x EDITOR /usr/bin/vim
+    set -x SHELL /usr/bin/fish
+    set -x GPG_TTY (tty)
+    keychain --eval --quiet -Q | source
+end
 
 # Start tmux automatically
-if status is-interactive
-and not set -q TMUX
-and set -q DISPLAY
-    pgrep tmux
-    if test "$status" -eq 0
-        exec tmux attach
-    else
-	    exec tmux
-    end
-end
+# Disable it because it may be confusing if I just want to open a new terminal.
+#if status is-interactive
+#and not set -q TMUX
+#and set -q DISPLAY
+#    pgrep tmux
+#    if test "$status" -eq 0
+#        exec tmux attach
+#    else
+#	    exec tmux
+#    end
+#end
